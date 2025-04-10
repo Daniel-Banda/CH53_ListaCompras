76c488c59cc960f50cea542622eb5ad868545d06
DBanda <danielbanda.stg@gmail.com>
2025-04-10 15:59:34 -0600

    Se agrega la funcionalidad para almacenar los datos en el local storage y mostrarlos al cargar nuevamente

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ const precioTotal = document.getElementById("precioTotal");
 let cont = 0;
 let costoTotal = 0;
 let totalEnProductos = 0;
+let datos = new Array() //almacena los elementos de la tabla
 
 function validarCantidad(){
     if(txtNumber.value.trim().length <= 0){
@@ -73,16 +74,61 @@ event.preventDefault();
                     <td>${txtNumber.value}</td>
                     <td>${precio}</td>
                     </tr>`;
+
+        let elemento = {
+                "cont" : cont,
+                "nombre" : txtName.value,
+                "cantidad" : txtNumber.value,
+                "precio" : precio
+        };
+        datos.push(elemento);
+
+        localStorage.setItem("datos", JSON.stringify(datos));
+
         cuerpoTabla.insertAdjacentHTML("beforeend", row)
         costoTotal += precio * Number(txtNumber.value)
         precioTotal.innerText= "$" + costoTotal.toFixed(2)
         contadorProductos.innerText = cont;
-
+        let resumen = {
+            "cont" : cont,
+            "totalEnProductos" : totalEnProductos,
+            "costoTotal" : costoTotal
+    };
+    localStorage.setItem("resumen", JSON.stringify(resumen))
        
        
         txtName.value = "";
         txtNumber.value = "";
         txtName.focus();
-
     } //if isValid
+
+})//btnAgregar
+
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    if(this.localStorage.getItem("datos")!=null){
+    datos = JSON.parse(this.localStorage.getItem("datos"));
+}//datos !=null
+datos.forEach((d) => {
+    let row =  `<tr>
+                    <td>${d.cont}</td>
+                    <td>${d.nombre}</td>
+                    <td>${d.cantidad}</td>
+                    <td>${d.precio}</td>
+                </tr>`;
+    cuerpoTabla.insertAdjacentHTML("beforeend", row);
+});
+
+if(this.localStorage.getItem("resumen")!=null){
+    let resumen = JSON.parse(this.localStorage.getItem("resumen"));
+    costoTotal = resumen.costoTotal;
+    totalEnProductos = resumen.totalEnProductos;
+    cont = resumen.cont;
+}
+
+precioTotal.innerText = "$ " + costoTotal.toFixed(2);
+productosTotal.innerText = totalEnProductos;
+contadorProductos.innerText = cont;
+
+
 })
